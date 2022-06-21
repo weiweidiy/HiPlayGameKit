5ecc4f6333dd868508809e426de64ce12270e001
weiweidiy <49595272@qq.com>
2022-06-21 19:04:09 +0800

    recode ILManager and support multi dll

---
+++ HotFixModuleTest/HotFixModuleTest/TestModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace HotFixModuleTest
+{
+    public class TestModule
+    {
+        public void Show(string value)
+        {
+            Debug.Log("IL HotFixModuleTest " + value);
+        }
+    }
+}
