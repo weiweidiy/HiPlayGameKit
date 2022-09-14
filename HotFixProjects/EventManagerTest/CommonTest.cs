4e629837502a6f53ae67a6c97084ab96f353d965
weiweidiy <49595272@qq.com>
2022-09-14 15:53:54 +0800

    update

---
+++ HotFixProjects/EventManagerTest/CommonTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace EventManagerTest
+{
+    [TestClass]
+    public class CommonTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
